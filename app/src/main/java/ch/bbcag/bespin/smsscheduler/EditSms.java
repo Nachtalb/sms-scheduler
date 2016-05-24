e38933bc11c5fdb0dea22604b9c19888f0e86a56
Hudson Muff <hudson.muff@unic.com>
2016-05-24 16:58:12 +0200

    implemented datePicker function

--- app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
@@ -1,17 +1,62 @@
 package ch.bbcag.bespin.smsscheduler;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class EditSms extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class EditSms extends AppCompatActivity implements View.OnClickListener {
+
+    // UI Reference
+    private EditText date;
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_sms);
+
+        dateFormatter = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
+        findViewsById();
+        setDateTimeField();
+
     }
+
+    private void findViewsById() {
+        date = (EditText) findViewById(R.id.date);
+        assert date != null;
+        date.setInputType(InputType.TYPE_NULL);
+        date.requestFocus();
+    }
+
+    private void setDateTimeField() {
+        date.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                date.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,13 +74,34 @@ public class EditSms extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.save:
-//                save();
+//                saveSms();
                 return true;
             case R.id.delete:
-//                delete();
+//                deleteSms();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+//    public void saveSms() {
+//
+//    }
+//
+//    public void deleteSms() {
+//
+//    }
+
+    public void onClickTimePicker(View view) {
+
+    }
+
+    public void onClickDatePicker(View view) {
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        datePickerDialog.show();
     }
 }
