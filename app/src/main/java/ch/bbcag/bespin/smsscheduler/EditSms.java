5e2c157f940add3ffd6dfce5ebd72a282523a2f7
nicku8 <nickomator@gmail.com>
2016-05-24 10:35:36 +0200

    added edit sms

--- app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
@@ -1,13 +1,13 @@
 package ch.bbcag.bespin.smsscheduler;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EditSms extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_sms);
     }
 }
