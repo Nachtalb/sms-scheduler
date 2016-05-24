ff6fc501e3d1275d157fb71aab18f2efe0a4c06f
Hudson Muff <hudson.muff@unic.com>
2016-05-24 14:48:02 +0200

    created edit sms layout no functionality yet

--- app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
@@ -2,6 +2,8 @@ package ch.bbcag.bespin.smsscheduler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditSms extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class EditSms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_sms);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_sms, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.save:
+//                save();
+                return true;
+            case R.id.delete:
+//                delete();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
