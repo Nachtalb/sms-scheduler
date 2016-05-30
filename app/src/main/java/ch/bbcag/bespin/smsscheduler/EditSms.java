3d01fa232358198524701f3398c5877c6a3d6596
Hudson Muff <hudson.muff@unic.com>
2016-05-30 11:45:57 +0200

    implemented timePicker function

--- app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
@@ -2,7 +2,6 @@ package ch.bbcag.bespin.smsscheduler;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,41 +11,79 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class EditSms extends AppCompatActivity implements View.OnClickListener {
+public class EditSms extends AppCompatActivity {
 
-    // UI Reference
+    // UI References
     private EditText date;
+    private EditText time;
 
     private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
     private SimpleDateFormat dateFormatter;
+    private SimpleDateFormat timeFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_sms);
 
+        timeFormatter = new SimpleDateFormat("HH:mm", Locale.GERMAN);
         dateFormatter = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
-        findViewsById();
-        setDateTimeField();
 
+        findViewsById();
+        setTimeField();
+        setDateField();
     }
 
     private void findViewsById() {
+        // get time field
+        time = (EditText) findViewById(R.id.time);
+        assert time != null;
+        time.setInputType(InputType.TYPE_NULL);
+        time.requestFocus();
+
+        // get date field
         date = (EditText) findViewById(R.id.date);
         assert date != null;
         date.setInputType(InputType.TYPE_NULL);
         date.requestFocus();
     }
 
-    private void setDateTimeField() {
-        date.setOnClickListener(this);
+    private void setTimeField() {
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.show();
+            }
+        });
 
-        Calendar newCalendar = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance();
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Calendar newTime = Calendar.getInstance();
+                newTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                newTime.set(Calendar.MINUTE, minute);
+                time.setText(timeFormatter.format(newTime.getTime()));
+            }
+        }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+    }
+
+    private void setDateField() {
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        Calendar calendar = Calendar.getInstance();
         datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -54,7 +91,7 @@ public class EditSms extends AppCompatActivity implements View.OnClickListener {
                 newDate.set(year, monthOfYear, dayOfMonth);
                 date.setText(dateFormatter.format(newDate.getTime()));
             }
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
     }
 
 
@@ -92,16 +129,11 @@ public class EditSms extends AppCompatActivity implements View.OnClickListener {
 //
 //    }
 
-    public void onClickTimePicker(View view) {
-
+    public void onClickTimeButton(View view) {
+        timePickerDialog.show();
     }
 
-    public void onClickDatePicker(View view) {
-        datePickerDialog.show();
-    }
-
-    @Override
-    public void onClick(View v) {
+    public void onClickDateButton(View view) {
         datePickerDialog.show();
     }
 }
