4254197b12a18b4dd7ea76181ca8c16d064068fb
nicku8 <nickomator@gmail.com>
2016-05-31 12:54:48 +0200

    directly add entry on add entry button

--- app/src/main/java/ch/bbcag/bespin/smsscheduler/MainActivity.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/MainActivity.java
@@ -44,12 +44,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Create Scheduled SMS", Snackbar.LENGTH_LONG).setAction("New", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        newEditSmsView();
-                    }
-                }).show();
+                newEditSmsView();
             }
         });
     }
