caea2a72cdefcede904463f4136cbc28da54ceab
Hudson Muff <hudson.muff@unic.com>
2016-06-06 16:13:06 +0200

    small refactoring

--- app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/EditSms.java
@@ -24,7 +24,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.UUID;
 
 public class EditSms extends AppCompatActivity implements View.OnClickListener {
 
--- app/src/main/java/ch/bbcag/bespin/smsscheduler/MainActivity.java
+++ app/src/main/java/ch/bbcag/bespin/smsscheduler/MainActivity.java
@@ -10,7 +10,6 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
